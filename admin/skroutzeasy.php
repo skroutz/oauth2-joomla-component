31774e9120e5c538e6a36da74f671319e905ce31
Chrisovalantis Kefalidis <valantis@skroutz.gr>
2015-06-22 11:39:04 +0300

    Update admin SkroutzEasyController for Joomla! version 3.x

--- admin/skroutzeasy.php
+++ admin/skroutzeasy.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package    SkroutzEasy component for Joomla 1.5.x and 1.6.x
+ * @package    SkroutzEasy component for Joomla 2.5.x and 3.x
  * @copyright  Copyright (c) 2012 Skroutz S.A. - http://www.skroutz.gr
  * @link       http://developers.skroutz.gr/oauth2
  * @license    MIT
@@ -10,11 +10,11 @@
 defined ('_JEXEC') or die ('Restricted access');
 
 // Require the base controller
-require_once(JPATH_COMPONENT.DS.'controller.php');
+require_once(JPATH_COMPONENT.DIRECTORY_SEPARATOR.'controller.php');
 
 // Require specific controller if requested
 if($controller = JRequest::getWord('controller')) {
-	$path = JPATH_COMPONENT.DS.'controllers'.DS.$controller.'.php';
+	$path = JPATH_COMPONENT.DIRECTORY_SEPARATOR.'controllers'.DIRECTORY_SEPARATOR.$controller.'.php';
 	if (file_exists($path)) {
 		require_once $path;
 	} else {
